15e5f1ac9c645b9b4965e476dbb31e714cbb4ff1
Cyrille Colin <cyrille.colin@univ-lorraine.fr>
2017-02-04 22:37:21 +0100

    add /logout and cookie stuff

--- server.js
+++ server.js
@@ -1,25 +1,60 @@
 import bodyParser from 'body-parser';
 import uuid from 'node-uuid';
 import express from 'express';
+import cookieParser from 'cookie-parser'
 
 var app = express();
+app.use(cookieParser());
 
-var tickets={};
+var tickets = {};
+var cookies = {};
 
-app.get('/login', function (req, res) {
-  res.sendFile(__dirname + '/form.html');
+app.get('/login', function(req, res) {
+  if (req.cookies['fake-cas'] && cookies[req.cookies['fake-cas']]) {
+    if (req.query.service) {
+      var ticket = "ST-" + uuid.v1();
+      tickets[ticket] = cookies[req.cookies['fake-cas']];
+      res.writeHead(302, {
+        'Location': `${req.query.service}?ticket=${ticket}`
+      });
+      res.end();
+    } else {
+      res.sendFile(__dirname + '/already.html');
+    }
+  } else {
+    res.sendFile(__dirname + '/form.html');
+  }
 });
 
-app.post('/login', bodyParser.urlencoded(),function (req, res) {
-  var ticket = "ST-" + uuid.v1();
-  tickets[ticket] = req.body.login;
-  console.log(req.body, req.query);
-  res.writeHead(302, {'Location': `${req.query.service}?ticket=${ticket}`});
+app.post('/login', bodyParser.urlencoded(), function(req, res) {
+  var cookie = uuid.v1();
+  cookies[cookie] = req.body.login;
+  res.cookie('fake-cas', cookie);
+  if (req.query.service) {
+    var ticket = "ST-" + uuid.v1();
+    tickets[ticket] = req.body.login;
+    res.writeHead(302, {
+      'Location': `${req.query.service}?ticket=${ticket}`
+    });
+    res.end();
+  } else {
+    res.sendFile(__dirname + '/already.html');
+  }
+});
+
+app.use('/logout', function(req, res) {
+  if (req.cookies['fake-cas'] && cookies[req.cookies['fake-cas']]) {
+    delete cookies[req.cookies['fake-cas']];
+  }
+  res.cookie('fake-cas', '');
+  res.writeHead(302, {
+    'Location': '/login'
+  });
   res.end();
 });
 
-app.get('/validate', function (req, res) {
-  if(tickets[req.query.ticket]) {
+app.get('/validate', function(req, res) {
+  if (tickets[req.query.ticket]) {
     res.send(`yes\n${tickets[req.query.ticket]}`);
     delete tickets[req.query.ticket];
   } else {
@@ -28,6 +63,6 @@ app.get('/validate', function (req, res) {
   res.end();
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Fake-cas available on http://localhost:3000');
 });
